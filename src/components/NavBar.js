5745f16953792886e4dd01ebfe86d9dbb71a5e3f
Trestin <trestinishak@gmail.com>
2021-08-31 16:16:02 -0500

    commit tree 43c7bab441ff3722905383268fa714f46477af44

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    {links.map((link, id) => {
+      console.log(link, links[id])
+      return <a key={id} href={"#" + link}>{link}</a>})
+    }
+  </nav>;
 }
 
 export default NavBar;
